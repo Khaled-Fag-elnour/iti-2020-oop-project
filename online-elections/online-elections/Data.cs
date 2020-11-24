03c219721e9d98066215e5ea216560550457a61f
Mohamed osama <61196430+mooka95@users.noreply.github.com>
2020-11-24 14:37:51 +0200

    Update Data.cs

--- online-elections/online-elections/Data.cs
+++ online-elections/online-elections/Data.cs
@@ -12,16 +12,16 @@ namespace online-elections{
             new Candidate(5,"Hana Elsaied"),
         };
         private  List<Citizen> CitizenList = new List<Citizen>(){
-            new Citizen(1,"Mostafa",30,false,false),
-            new Citizen(2,"Mohamed",16,false,false),
-            new Citizen(3,"Ahmed",18,false,true),
-            new Citizen(4,"Omar",20,true,false),
-            new Citizen(5,"Lila",42,true,false),
-            new Citizen(6,"Esraa",60,false,false),
-            new Citizen(7,"Mahmoud",27,false,true),
-            new Citizen(8,"Alaa",33,false,true),
-            new Citizen(9,"Samar",17,false,false),
-            new Citizen(10,"Maged",19,false,false),
+            new Citizen(1,"Mostafa",30,false),
+            new Citizen(2,"Mohamed",16,false),
+            new Citizen(3,"Ahmed",18,true),
+            new Citizen(4,"Omar",20,false),
+            new Citizen(5,"Lila",42,false),
+            new Citizen(6,"Esraa",60,false),
+            new Citizen(7,"Mahmoud",27,true),
+            new Citizen(8,"Alaa",33,true),
+            new Citizen(9,"Samar",17,false),
+            new Citizen(10,"Maged",19,false),
         };
 
         public List<Citizen> getCitizenList(){
